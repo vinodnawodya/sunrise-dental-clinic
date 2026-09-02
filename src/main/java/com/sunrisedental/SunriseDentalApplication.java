1d2e3d2ad2c76c86887a8c208d0b851aa36eea70
ACER <vinodnawodya7@gmail.com>
2026-09-02 14:20:21 +0530

    chore: project scaffold

---
+++ src/main/java/com/sunrisedental/SunriseDentalApplication.java
@@ -0,0 +1,13 @@
+package com.sunrisedental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseDentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunriseDentalApplication.class, args);
+    }
+
+}
